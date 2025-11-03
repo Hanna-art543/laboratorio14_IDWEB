e3852624e41b64e92945837b03b5387352a94496
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-03 16:45:21 -0500

    Subir ejercicio05.js

---
+++ ejercicios/ejercicio05.js
@@ -0,0 +1,22 @@
+//Funcion de grados a radianes
+function gradosARadianes (grados) {
+    let radianes = grados * (Math.PI/180);
+    let seno = Math.sin(radianes);
+    let coseno = Math.cos(radianes);
+
+    return "\nGrado en radianes: "+radianes +
+    "\nSeno: "+ seno +
+    "\nCoseno: "+ coseno
+};
+
+//Funcion de radianes a grados
+function radianesAGrados (radianes) {
+    let grados = radianes * (180/Math.PI);
+    return radianes + "en grados " + grados;
+}
+
+//Ingresar grados y mostrar
+console.log(gradosARadianes(90));
+
+//Ingresar radianes y mostrar
+console.log(radianesAGrados(0.5));
